dfd11dfb2b99d24a5a095e603f60f5c8f8087251
Alexander J Ray <alexjamesray@gmail.com>
2017-03-22 14:03:43 -0700

    Adds dashboards inital flight search form semantics and styles

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import $ from 'jquery';
 
-console.log($);
-
 class Dashboard extends React.Component {
 	render () {
 		return (
-			<h1>Skurt Interview!!!</h1>
+			<section className='vertical-horizontal-center'>
+				<div className='container-fluid container'>
+					<div className='row text-center dashboard-logo'>
+						<img src='images/logo.png' class="img-responsive" alt="Skurt"/>
+					</div>
+					<form role="form">
+						<div className="row form-group">
+							<div className='col-sm-6 col-sm-offset-3'>
+								<input type="text" className="form-control" placeholder="Search Flight" />
+							</div>
+						</div>
+
+						<div className='row text-center dashboard-form-action-group'>
+							<button type="submit" className="btn btn-default">Search</button>
+						</div>
+					</form>
+				</div>
+			</section>
 		);
 	}
 }
